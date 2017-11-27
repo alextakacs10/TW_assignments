674176f3a70f290f3876f14643a2341c24eac372
Alex Takács <alextakacs10@gmail.com>
2017-11-27 10:41:13 +0100

    Created properties and constructor for Person class.

---
+++ Serializer/Serializer/Form1.Designer.cs
@@ -0,0 +1,151 @@
+﻿namespace Serializer
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.NameLabel = new System.Windows.Forms.Label();
+            this.AddressLabel = new System.Windows.Forms.Label();
+            this.PhoneLabel = new System.Windows.Forms.Label();
+            this.NameBox = new System.Windows.Forms.TextBox();
+            this.AddressBox = new System.Windows.Forms.TextBox();
+            this.PhoneBox = new System.Windows.Forms.TextBox();
+            this.PreviousButton = new System.Windows.Forms.Button();
+            this.NextButton = new System.Windows.Forms.Button();
+            this.SaveButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // NameLabel
+            // 
+            this.NameLabel.AutoSize = true;
+            this.NameLabel.Location = new System.Drawing.Point(13, 13);
+            this.NameLabel.Name = "NameLabel";
+            this.NameLabel.Size = new System.Drawing.Size(35, 13);
+            this.NameLabel.TabIndex = 0;
+            this.NameLabel.Text = "Name";
+            // 
+            // AddressLabel
+            // 
+            this.AddressLabel.AutoSize = true;
+            this.AddressLabel.Location = new System.Drawing.Point(13, 60);
+            this.AddressLabel.Name = "AddressLabel";
+            this.AddressLabel.Size = new System.Drawing.Size(45, 13);
+            this.AddressLabel.TabIndex = 1;
+            this.AddressLabel.Text = "Address";
+            // 
+            // PhoneLabel
+            // 
+            this.PhoneLabel.AutoSize = true;
+            this.PhoneLabel.Location = new System.Drawing.Point(13, 107);
+            this.PhoneLabel.Name = "PhoneLabel";
+            this.PhoneLabel.Size = new System.Drawing.Size(38, 13);
+            this.PhoneLabel.TabIndex = 2;
+            this.PhoneLabel.Text = "Phone";
+            // 
+            // NameBox
+            // 
+            this.NameBox.Location = new System.Drawing.Point(16, 30);
+            this.NameBox.Name = "NameBox";
+            this.NameBox.Size = new System.Drawing.Size(227, 20);
+            this.NameBox.TabIndex = 3;
+            // 
+            // AddressBox
+            // 
+            this.AddressBox.Location = new System.Drawing.Point(16, 76);
+            this.AddressBox.Name = "AddressBox";
+            this.AddressBox.Size = new System.Drawing.Size(227, 20);
+            this.AddressBox.TabIndex = 4;
+            // 
+            // PhoneBox
+            // 
+            this.PhoneBox.Location = new System.Drawing.Point(16, 123);
+            this.PhoneBox.Name = "PhoneBox";
+            this.PhoneBox.Size = new System.Drawing.Size(227, 20);
+            this.PhoneBox.TabIndex = 5;
+            // 
+            // PreviousButton
+            // 
+            this.PreviousButton.Location = new System.Drawing.Point(16, 163);
+            this.PreviousButton.Name = "PreviousButton";
+            this.PreviousButton.Size = new System.Drawing.Size(68, 23);
+            this.PreviousButton.TabIndex = 6;
+            this.PreviousButton.Text = "Previous";
+            this.PreviousButton.UseVisualStyleBackColor = true;
+            // 
+            // NextButton
+            // 
+            this.NextButton.Location = new System.Drawing.Point(175, 162);
+            this.NextButton.Name = "NextButton";
+            this.NextButton.Size = new System.Drawing.Size(68, 23);
+            this.NextButton.TabIndex = 7;
+            this.NextButton.Text = "Next";
+            this.NextButton.UseVisualStyleBackColor = true;
+            // 
+            // SaveButton
+            // 
+            this.SaveButton.Location = new System.Drawing.Point(16, 199);
+            this.SaveButton.Name = "SaveButton";
+            this.SaveButton.Size = new System.Drawing.Size(227, 23);
+            this.SaveButton.TabIndex = 8;
+            this.SaveButton.Text = "Save";
+            this.SaveButton.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(263, 246);
+            this.Controls.Add(this.SaveButton);
+            this.Controls.Add(this.NextButton);
+            this.Controls.Add(this.PreviousButton);
+            this.Controls.Add(this.PhoneBox);
+            this.Controls.Add(this.AddressBox);
+            this.Controls.Add(this.NameBox);
+            this.Controls.Add(this.PhoneLabel);
+            this.Controls.Add(this.AddressLabel);
+            this.Controls.Add(this.NameLabel);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label NameLabel;
+        private System.Windows.Forms.Label AddressLabel;
+        private System.Windows.Forms.Label PhoneLabel;
+        private System.Windows.Forms.TextBox NameBox;
+        private System.Windows.Forms.TextBox AddressBox;
+        private System.Windows.Forms.TextBox PhoneBox;
+        private System.Windows.Forms.Button PreviousButton;
+        private System.Windows.Forms.Button NextButton;
+        private System.Windows.Forms.Button SaveButton;
+    }
+}
+
---
+++ Serializer/Serializer/Person.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Serializer
+{
+    [Serializable]
+    class Person
+    {
+        public static int serial = 0;
+        public string Name { get; set; }
+        public string Address { get; set; }
+        public string PhoneNumber { get; set; }
+        public DateTime RecordTime { get; set; }
+        [NonSerialized] public string serialNumber;
+
+        public string SerialNumber
+        {
+            get { return serialNumber; }
+            set { serialNumber = value; }
+        }
+
+        public Person()
+        {
+
+        }
+
+        public Person(string name, string address, string phoneNumber, DateTime recordtime)
+        {
+            Name = name;
+            Address = address;
+            PhoneNumber = phoneNumber;
+            RecordTime = recordtime;
+            SerialNumber = (serial + 1).ToString();
+        }
+        
+    }
+}
